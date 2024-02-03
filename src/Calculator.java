32fc7da9bc74784687d3bef494fb7353ffde2c71
Impulse12600g <k.lal.1078780@gmail.com>
2024-02-02 22:18:21 -0700

    First Commit

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,33 @@
-public class Calculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Calculator implements ActionListener{
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons = new JButton[10];// Impliments an array of numbers to press
+    JButton[] functionButtons = new JButton[8];// functions
+    JButton addButton, subButton, mulButton, divButton,
+            decButton, equButton, delButton, clrButton;
+    JPanel panel;
+
+    Font myFont = new Font("Ink Free", Font.BOLD, 30);
+
+    double num1 =  0, num2 = 0, num3 = 0;
+
+
+    Calculator(){
+
+    }
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
